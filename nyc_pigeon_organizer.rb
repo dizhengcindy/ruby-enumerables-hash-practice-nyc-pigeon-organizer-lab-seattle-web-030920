6820f06d96065efb7e221c87c899e9554261f471
Di Zheng <dizhengcindy@gmail.com>
2020-02-28 19:54:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,7 +9,7 @@ def nyc_pigeon_organizer(data)
     
       array.each do |name|
         #key2 still a key not string
-        binding.pry
+        
         if newHash[name][key1]
           newHash[name][key1].push(key2.to_s) 
         else
