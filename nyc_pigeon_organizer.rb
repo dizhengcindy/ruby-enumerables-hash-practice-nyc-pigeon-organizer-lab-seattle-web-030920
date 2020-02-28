fca0762cc1e5a4a46d82a86905a5c185642a2b05
Di Zheng <dizhengcindy@gmail.com>
2020-02-28 19:30:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  newHash = {}
+  
+  data.reduce({}) do |memo1, (key1,hash)| 
+
+    hash.reduce({}) do |memo2, (key2,array)|
+    
+      array.each do |name|
+        if newHash[name][key1]
+        
+          newHash[name][key1].push(key2) 
+        else
+          
+        newHash[name][key1] = key2 
+        end
+      end
+      
+      p memo2
+  end
+ p memo1
+end    
+newHash
+  
 end
+
+puts nyc_pigeon_organizer(data)
